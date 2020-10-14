fe73dc6d9cb7db7506eeaf1cb69e4baca6e8494c
luca <beccibrian@gmail.com>
2020-10-14 17:13:19 -0300

    fix: error handlings and folder helpers with functions for check camps and parameters added

--- src/controllers/photo.controller.ts
+++ src/controllers/photo.controller.ts
@@ -2,67 +2,162 @@ import { json, Request, Response } from 'express'
 import path from 'path'
 import fs from 'fs-extra'
 
-
+//helpers for checks and more functions 
+import { bodyCheck, createCheckCamps, IDCheck } from "../helpers/photo.checks";
+//schema import
 import Photo from '../models/Photo'
 
 export const createPhoto = async(req: Request, res: Response): Promise<Response> => {
-    const { title, description } = req.body;
-    const newPhoto = {
-        title,
-        description,
-        imagePath: req.file.path
+    try {
+        const { title, description } = req.body;
+        const newPhoto = {
+            title,
+            description,
+            imagePath: req.file.path
+        }
+        const checked = createCheckCamps(title, description, req.file.path)
+        if(checked === true){
+            return res.json({
+                petition: false,
+                message: 'Check all camps and try again'
+            })
+        }
+        const photo = await new Photo(newPhoto)
+        await photo.save();
+
+        return res.json({
+            petition: true,
+            photocreated: photo
+        })
     }
-
-    const photo = await new Photo(newPhoto)
-    await photo.save();
-
-    return res.json({
-        message: 'Photo successfully saved.',
-        photo
-    })
+    catch(e) {
+        console.log(e)
+        return res.status(500).json({
+            petition: false,
+            message: 'Server error, try again later.'
+        })
+    }
 }
 
 export const getPhotos = async(req: Request, res: Response): Promise<Response> => {
-    const photos = await Photo.find();
-    return res.json({
-        data: [photos]
-    })
+    try{
+        const photos = await Photo.find();
+        return res.json({
+            petition: true,
+            data: [photos]
+        })
+    }
+    catch(e){
+        console.log(e)
+        return res.status(500).json({
+            petition: false,
+            message: 'Server error, try again later.'
+        })
+    }
 }
 
 export const getPhoto =  async(req: Request, res: Response): Promise<Response> => {
-    const id: string = req.params.id
-    const photo = await Photo.findById(id)
-    return res.json({
-        data: photo
-    })
+    try{
+        const id: string = req.params.id
+        const checked = IDCheck(id)
+        if(checked){
+            return res.json({
+                data: false,
+                message: 'ID short, try again with correct ID.'
+            })
+        }
+        try{
+            const photo = await Photo.findById(id)
+            return res.json({
+                petition: true,
+                data: photo
+            })
+        }
+        catch(e){
+            return res.status(400).json({ 
+                petition: false,
+                message: 'ID invalid, try again with correct ID'
+            })
+        }
+        }
+    catch(e){
+        return res.status(500).send('Internal server error, try again later.')
+    }
 }
 
 export const deletePhoto = async(req: Request, res: Response): Promise<Response> => {
-    const id: string = req.params.id;
-    const photo = await Photo.findByIdAndDelete(id);
-    if(photo){
-        await fs.unlink(path.resolve(photo.imagePath))
+    try{
+        const id: string = req.params.id;
+        const checked = IDCheck(id)
+        if(checked){
+            return res.json({
+                data: false,
+                message: 'ID short, try again with correct ID.'
+            })
+        }
+        const photo = await Photo.findByIdAndDelete(id);
+        if(photo){
+            await fs.unlink(path.resolve(photo.imagePath))
+            return res.json({
+                petition: true,
+                photodeleted: photo
+            })
+        }
         return res.json({
-            deleted: true,
-            photo
+            petition: false,
+            message: 'ID image not found'
+        }) 
+    } 
+    catch(e){
+        console.log(e)
+        return res.status(500).json({
+            petition: false,
+            message: 'Internal server error, try again later.'
         })
     }
-    return res.json({
-        deleted: false,
-        message: 'ID image not found'
-    })
-    
 }
 
 export const updatePhoto = async(req: Request, res: Response): Promise<Response> => {
-    const id = req.params.id;
-    const {title, description} = req.body;
-    const updatedPhoto = await Photo.findByIdAndUpdate(id, {
-        title,
-        description
-    }, {new: true})
-    return res.json({
-        message: 'Updated successfully',
-        updatedPhoto
-    })
+    try{
+        const id = req.params.id;
+        const {title, description} = req.body;
+        const idChecked = IDCheck(id)
+        const bodyChecked = bodyCheck(title, description)
+
+        if(idChecked == true){
+            return res.status(400).json({
+                petition: false,
+                message: 'ID short, try again with complete ID.'
+            })
+        }else if(bodyChecked == true){
+            return res.status(400).json({
+                petition: false,
+                message: 'title or description not found, please fill in the parameters'
+            })
+        }
+
+        try{
+            const updatedPhoto = await Photo.findByIdAndUpdate(id, {
+                title,
+                description
+            }, {new: true})
+            return res.json({
+                petition: true,
+                updatedPhoto
+            })
+        }
+        catch{
+            return res.status(400).json({
+                petition: false,
+                message: 'ID invalid, please use the correct ID.'
+            })
+        }
+    }
+    catch(e){
+        console.log(e)
+        return res.status(500).json({
+            petition: false,
+            message: 'Server internal erro, please try later.'
+        })
+    }
 }
